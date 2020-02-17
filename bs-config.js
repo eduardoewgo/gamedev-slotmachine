a3f58b30c3953ee2e1ada74dda335f9189e1c474
Eduardo <eduardo.ewgo@gmail.com>
2020-02-17 16:22:53 -0500

    Trying to understand Heroku(again)

--- bs-config.js
+++ bs-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    files: ['./**/*.{html,htm,css,js}'],
-    server:{
+    files: ['./**/*.{html,htm,css,js,png,wav}'],
+    server: {
         baseDir: "./"
     }
 };
